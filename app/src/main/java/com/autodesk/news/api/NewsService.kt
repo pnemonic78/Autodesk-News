23a9ba3ee9df72cbfc039900d7184ed74515eb6b
Moshe <pnemonic@gmail.com>
2018-12-18 23:23:32 +0200

    responses from server are usually once-off

--- app/src/main/java/com/autodesk/news/api/NewsService.kt
+++ app/src/main/java/com/autodesk/news/api/NewsService.kt
@@ -2,7 +2,7 @@ package com.autodesk.news.api
 
 import com.autodesk.news.BuildConfig
 import com.autodesk.news.model.api.ArticlesResponse
-import io.reactivex.Observable
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.*
@@ -14,12 +14,12 @@ interface NewsService {
 
     @GET("articles")
     fun getArticles(@Query("source") source: String,
-                    @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY): Observable<ArticlesResponse>
+                    @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY): Single<ArticlesResponse>
 
     @GET("top-headlines")
     fun getTopHeadlines(@Query("country") country: String? = null,
                         @Query("language") language: String? = Locale.getDefault().language,
                         @Query("sources") sources: String? = null,
                         @Query("page") page: Int? = null,
-                        @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY): Observable<ArticlesResponse>
+                        @Query("apiKey") apiKey: String = BuildConfig.NEWS_API_KEY): Single<ArticlesResponse>
 }
