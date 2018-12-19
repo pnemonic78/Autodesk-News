8af2cbe586041446de3e99468dd9e0d5275ca0db
Moshe <pnemonic@gmail.com>
2018-12-19 23:00:36 +0200

    format

--- app/src/main/java/com/autodesk/news/model/api/ArticlesResponse.kt
+++ app/src/main/java/com/autodesk/news/model/api/ArticlesResponse.kt
@@ -6,16 +6,16 @@ import com.google.gson.annotations.SerializedName
  * List of articles.
  */
 class ArticlesResponse(
-        @SerializedName("status")
-        val status: String,
-        @SerializedName("code")
-        val code: String? = null,
-        @SerializedName("message")
-        val message: String? = null,
-        @SerializedName("totalResults")
-        val totalResults: Int,
-        @SerializedName("articles")
-        val articles: List<NewsArticle>
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("code")
+    val code: String? = null,
+    @SerializedName("message")
+    val message: String? = null,
+    @SerializedName("totalResults")
+    val totalResults: Int,
+    @SerializedName("articles")
+    val articles: List<NewsArticle>
 ) {
     companion object {
         const val STATUS_OK = "ok"
